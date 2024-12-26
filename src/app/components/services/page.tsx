11b307e89b8d41643de95d180cd28b60834f30d9
Muhammad-Hassan123 <muhammadhassan3172003@gmail.com>
2024-12-26 21:52:16 +0500

    Initial commit

---
+++ src/app/components/services/page.tsx
@@ -0,0 +1,48 @@
+'use client'
+import React, { useEffect, useState } from "react";
+import ServicesCard from "../sercivesCard/page";
+import { client } from "@/sanity/lib/client";
+import { Service } from "../../../../types/types";
+
+function ProjectPage() {
+  const [projects, setProjects] = useState<Service[]>([]); // Initialize with an empty array
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await client.fetch(`*[_type == "servicesSchema"]`);
+        setProjects(data); // Set the projects in context
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log("PP: ", projects);
+
+  return (
+    <div>
+      <div className="grid min-h-[70vh] grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mx-4 lg:mx-20 m-28">
+        {projects.length > 0 ? (
+          projects.map((project: Service, index: number) => (
+            <ServicesCard
+              key={index}
+              name={project.name}
+              description={project.description}
+              image={project.image}
+              slug={project.slug?.current}
+            />
+          ))
+        ) : (
+          <div className="flex items-center justify-center w-full text-xl font-bold text-blue-500">
+            Loading...
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default ProjectPage;
---
+++ types/types.tsx
@@ -0,0 +1,70 @@
+export type Project = {
+  name: string;
+  description: string;
+  image: {
+    asset: {
+      _ref: string;
+      url: string;
+    };
+  };
+  slug: {
+    current: string
+    _type: string
+  };
+  detail: string
+};
+
+export type Service = {
+  name: string;
+  description: string;
+  image: {
+    asset: {
+      _ref: string;
+      url: string;
+    };
+  };
+  slug: {
+    current: string
+    _type: string
+  };
+  detail: string
+};
+
+
+
+export type ServicesCardProp = {
+  name: string;
+  description: string;
+  image: {
+    asset: {
+      _ref: string;
+      url: string;
+    };
+  };
+  slug: string;
+};
+
+export type ProjectCardProps = {
+  name: string;
+  description: string;
+  image: {
+    asset: {
+      _ref: string;
+      url: string;
+    };
+  };
+  slug: string;
+};
+
+
+// Type for the asset (Sanity Image Object)
+export type ImageAsset = {
+  _ref: string;
+  url: string;
+};
+
+// Type for the source parameter in urlFor
+export type ImageSource = {
+  _ref: string;
+  url: string;
+};
