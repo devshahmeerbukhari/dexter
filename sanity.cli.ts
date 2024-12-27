cc53cc130557a294232ba2a44eef278baa5ab064
Muhammad-Hassan123 <muhammadhassan3172003@gmail.com>
2024-12-27 16:02:53 +0500

    added variable sanity.cli.ts

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -4,7 +4,8 @@
 **/
 import { defineCliConfig } from 'sanity/cli'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const projectId = "hkgtynx0"
+const dataset = "production"
+const NEXT_PUBLIC_SANITY_API_VERSION = "2024-12-24"
 
 export default defineCliConfig({ api: { projectId, dataset } })
