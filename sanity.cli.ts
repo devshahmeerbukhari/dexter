4d7534568723c95fe736ad6520e1690b6e561fcf
Muhammad-Hassan123 <muhammadhassan3172003@gmail.com>
2024-12-27 16:19:59 +0500

    added NEXT_PUBLIC_SANITY_API_VERSION in sanit.cli.ts

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -4,8 +4,8 @@
 **/
 import { defineCliConfig } from 'sanity/cli'
 
-const projectId = "hkgtynx0"
-const dataset = "production"
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 const NEXT_PUBLIC_SANITY_API_VERSION = "2024-12-24"
 
 export default defineCliConfig({ api: { projectId, dataset } })
